2802bf214802a11f57afad86f618fc9e5d3f77e6
Mike Santiago <miketheripper1@gmail.com>
2016-04-02 20:07:14 -0400

    lol oops

---
+++ Minecraft2DRebirth/Maps/MinecraftMap.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework;
+using Minecraft2DRebirth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft2DRebirth.Maps
+{
+    public class MinecraftMap : IMap
+    {
+        public MapMetadata Metadata { get; set; }
+
+        /// <summary>
+        /// The tiles contained in the background layer of the map.
+        /// </summary>
+        public ITile[,] BackgroundTileMap { get; set; }
+
+        /// <summary>
+        /// The tiles contained in the foreground layer of the map.
+        /// </summary>
+        public ITile[,] TileMap { get; set; }
+
+        public MinecraftMap()
+        {
+            Metadata = new MapMetadata
+            {
+                MapName = "World 1",
+                Width = 256,
+                Height = 256, //256 x 256 cube
+            };
+            TileMap = new ITile[Metadata.Height, Metadata.Width];
+        }
+
+        public void GenerateTestMap()
+        {
+            for(int y = 0; y < Metadata.Width; y++)
+            {
+                for(int x = 0; x < Metadata.Height; x++)
+                {
+                    //if(y > 32)
+                        TileMap[y, x] = new StoneBlock { Position = new Vector2(x * Constants.TileSize, y * Constants.TileSize) };
+                }
+            }
+        }
+
+        public void Draw(Graphics.Graphics graphics)
+        {
+            for(int y = 0; y < Metadata.Width; y++)
+            {
+                for(int x = 0; x < Metadata.Height; x++)
+                {
+                    if (TileMap[y, x] != null)
+                        TileMap[y, x].Draw(graphics);
+                }
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            for (int y = 0; y < Metadata.Width; y++)
+            {
+                for (int x = 0; x < Metadata.Height; x++)
+                {
+                    if (TileMap[y, x] != null)
+                        TileMap[y, x].Update(gameTime);
+                }
+            }
+        }
+
+    }
+}
---
+++ Minecraft2DRebirth/Maps/StoneBlock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Minecraft2DRebirth.Graphics;
+
+namespace Minecraft2DRebirth.Maps
+{
+    /// <summary>
+    /// TODO Script this
+    /// </summary>
+    class StoneBlock : MinecraftBlock
+    {
+        public StoneBlock() : base()
+        {
+            Name = "minecraft:stone";
+            InBackground = false;
+            SheetName = "terrain";
+            TileIndex = new Vector2(0, 1 * Constants.SpriteSize);
+            Position = Vector2.Zero;
+            PreferredTool = ToolType.Pickaxe;
+            PlaceSoundName = "stone{0}";
+            Transparency = TileTransparency.FullyOpague;
+            Hardness = 2f;
+            Drops = Name;
+        }
+
+        public override void Draw(Graphics.Graphics graphics)
+        {
+            var texture = graphics.GetTexture2DByName(SheetName);
+            graphics.GetSpriteBatch().Draw(
+                texture, 
+                Position.ToRectangle(),
+                new Rectangle((int)TileIndex.X, (int)TileIndex.Y, Constants.SpriteSize, Constants.SpriteSize),
+                Color.White
+            );
+        }
+
+        public override void Update(GameTime gameTime)
+        { 
+        }
+    }
+}
