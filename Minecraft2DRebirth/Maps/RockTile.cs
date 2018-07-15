261686d6b8cc43ddca750361ff570dd4cd2ad3e2
Mike Santiago <miketheripper1@gmail.com>
2018-07-15 14:20:21 -0400

    in the middle of something

---
+++ Minecraft2DRebirth/Maps/RockTile.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using RockSolidEngine.Graphics;
+
+namespace RockSolidEngine.Maps
+{
+    public class RockTile : ITile
+    {
+        public RockTile()
+        {
+        }
+
+        public string SheetName { get; set; }
+        public Vector2 TileIndex { get; set; }
+        public TileTransparency Transparency { get; set; }
+        public Vector2 Position { get; set; }
+        public Vector2 SpriteSize { get; set; }
+        public string EntityName { get; set; }
+        public Rectangle Hitbox { get; set; }
+        public int Angle { get; internal set; }
+
+        public float BottomSide()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(Graphics.Graphics graphics)
+        {
+            
+        }
+
+        public float LeftSide()
+        {
+            throw new NotImplementedException();
+        }
+
+        public float RightSide()
+        {
+            throw new NotImplementedException();
+        }
+
+        public float TopSide()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Minecraft2DRebirth/Maps/RockTileMap.cs
+++ Minecraft2DRebirth/Maps/RockTileMap.cs
@@ -50,7 +50,7 @@ namespace RockSolidEngine.Maps
                     char readChar = reader.ReadChar();
                     returnValue += readChar.ToString();
                 }
-                reader.ReadChar(); //to skip over the null termination at the end of the string.
+                reader.ReadChar(); //to skip over the null termination at the end of the string, C# doesn't need it.
                 return returnValue.Trim();
             }
             else
@@ -92,6 +92,42 @@ namespace RockSolidEngine.Maps
                         this.Metadata = ReadMapMetadata;
 
                         TileMap = new ITile[ReadMapMetadata.Height, ReadMapMetadata.Width];
+
+                        int totalTiles = Metadata.Width * Metadata.Height;
+                        for (int i = 0; i < totalTiles; i++)
+                        {
+                            short id = reader.ReadInt16();
+                            short idLayer2 = -1;
+                            if (version >= 5)
+                                idLayer2 = reader.ReadInt16();
+                            short angle = 0;
+                            byte angleChar = reader.ReadByte();
+                            switch (angleChar)
+                            {
+                                case 1:
+                                    angle = 0;
+                                    break;
+                                case 2:
+                                    angle = 90;
+                                    break;
+                                case 3:
+                                    angle = 180;
+                                    break;
+                                case 4:
+                                    angle = 270;
+                                    break;
+                            }
+                            //TODO: an enum for tile collision? also TODO: check that tile collision isn't already a thing idk 
+                            byte collision = reader.ReadByte();
+
+                            Point pointIn2DArray = IndexToPoint(i, Metadata.Width, Metadata.Height);
+                            if (id >= 0)
+                            {
+                                ITile tile = new RockTile();
+                                TileMap[pointIn2DArray.Y, pointIn2DArray.X] = tile;
+                            }
+
+                        }
                     }
                     else
                         throw new Exception($"Wrong version in map file (Reading {VERSION}, map file was {version}).");
@@ -101,6 +137,14 @@ namespace RockSolidEngine.Maps
                     throw new Exception($"Wrong file type (Expected {HEADER_0}{HEADER_1}, got {header[0]}{header[1]}).");
                 }
             }
+        }
+
+        private Point IndexToPoint(int index, int width, int height)
+        {
+            int x = (index % width);
+            int y = (index / width);
+
+            return new Point(x, y);
         }
 
         public void Draw(Graphics.Graphics graphics, Camera2D camera = null)
